e0b29a6ff1a445a62a978cee1b5b0197f27ec994
nemf <nemf@users.noreply.github.com>
2024-12-18 20:22:08 +0900

    Update volup.sh

--- volup.sh
+++ volup.sh
@@ -1,20 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-TMP=hoho.m4a
-if [ -f $1 ]; then
-    mv $1 $TMP
-else
-    echo "File does not exist ( $1 )"
-    exit
+set -e  # エラー時に即座に終了
+
+# 関数定義
+cleanup() {
+    [[ -f "$TMP" ]] && rm "$TMP"
+}
+
+# 引数チェック
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
 fi
 
-TARGET=$1
+INPUT="$1"
+TMP="$(mktemp).m4a"
+trap cleanup EXIT
+
+if [[ ! -f "$INPUT" ]]; then
+    echo "File does not exist: $INPUT"
+    exit 1
+fi
+
+# 現在の日付をyyyymmdd形式で取得
+CURRENT_DATE=$(date +"%Y%m%d")
+
+# 入力ファイルの拡張子を取得
+EXTENSION="${INPUT##*.}"
+
+# 新しい出力ファイル名を生成
+OUTPUT="${CURRENT_DATE}-volup.${EXTENSION}"
+
+cp "$INPUT" "$TMP"
 
 echo "Detecting the current volume level"
-VOL=`ffmpeg -i $TMP -vn -af volumedetect -f null - 2>&1 | grep max_volume | awk '{print $5}'| sed 's/-//'`
+VOL=$(ffmpeg -i "$TMP" -vn -af volumedetect -f null - 2>&1 | 
+      grep max_volume | awk '{print $5}' | sed 's/-//')
 
 echo "max_volume=$VOL"
 
 echo "Started to adjust the volume ...."
-ffmpeg -loglevel error -i $TMP -af volume=${VOL}dB $TARGET 
-echo "Completed !!"
+ffmpeg -loglevel error -i "$TMP" -af "volume=${VOL}dB" "$OUTPUT"
+echo "Completed!! Output file: $OUTPUT"
